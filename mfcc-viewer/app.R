42c489ae236fe175e109549ca34b099bb23d3661
Griffin Going <griffingoing@Griffins-MacBook-Pro-2.local>
2021-02-28 14:07:18 -0500

    Beta v1

--- mfcc-viewer/app.R
+++ mfcc-viewer/app.R
@@ -4,6 +4,8 @@ library(tidyverse)
 library(seewave)
 library(tuneR)
 library(DT)
+library(shinycssloaders)
+library(shinyWidgets)
 
 convert_audio <- function(filePath, filename){
   
@@ -25,32 +27,44 @@ ui <- fluidPage(
   
   #hr(),
   
-  plotOutput('plot'),
+  plotOutput('plot') %>%
+    withSpinner(type = 8, color='#037bbc', size = 2),
   
   hr(),
   
   fluidRow(
     column(3,
            h4("File Management"),
+           hr(),
            fileInput('newFile', "Upload New .wav File"),
-           selectInput('ceps', 'Cepstrum', c("Any")),
-           
+           downloadButton('downloadFeatures', "Download Features"),
+           downloadButton('downloadPlot', "Download Plot"),
+           hr(),
+           h4("Geometry"),
+           hr(),
+           switchInput('smoothingLine', "Smoothing Line", value = FALSE, inline = TRUE),
+           switchInput('cepstrumShapes', "Cepstrum Shapes", value = FALSE, inline = TRUE),
+
+    ),
+    column(2,
+           h4("Graph Options"),
+           hr(),
+           selectInput('ceps', 'Cepstrum', c("All")),
+           sliderInput('alpha', "Alpha", min = 0, max = 100,
+                       value = 65, step = 5, ticks = FALSE),
+           sliderInput('xWindow', 'X Window', 
+                       min=0, max=0, value=c(0, 0), 
+                       step=1, round=0),
+           sliderInput('yWindow', 'Y Window', 
+                       min=0, max=0, value=c(0, 0), 
+                       step=1, round=0),
+    ),
+    column(3,
+           h4("Table of Differences"),
+           dataTableOutput('diffTable'),
     ),
     column(4,
-           sliderInput('xmin', 'X Window Min', 
-                       min=0, max=0, value=0, 
-                       step=1, round=0),
-           sliderInput('xmax', 'X Window Max', 
-                       min=0, max=0, value=0, 
-                       step=1, round=0),
-           sliderInput('ymin', 'Y Window Min', 
-                       min=0, max=0, value=0, 
-                       step=1, round=0),
-           sliderInput('ymax', 'Y Window Max', 
-                       min=0, max=0, value=0, 
-                       step=1, round=0)
-    ),
-    column(4,
+           h4("Extracted Features"),
            dataTableOutput('datasetTable')
     )
   )
@@ -59,7 +73,44 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
+  lastCeps <- "All"
+  
+  filter_data <- function (x, cepstrum, update = FALSE) {
+    
+    
+    if (cepstrum != "All") {
+      x <- x %>%
+        filter(ceps == cepstrum)
+    }
+    
+    if (update == TRUE) {
+      # check for new min/max values, and set the current xWindow right val if necessary
+      xMaxVal <- max(x$frame, na.rm = TRUE)
+      yMinVal <- floor(min(x$coeff, na.rm = TRUE))
+      yMaxVal <- ceiling(max(x$coeff, na.rm = TRUE))
+      if (input$xWindow[2] == 0) { input$xWindow[2] <- xMaxVal } 
+      
+      print(c(0, xMaxVal))
+      print(c(yMinVal, yMaxVal))
+      
+      
+      # only alter values to hard min/max if we're switching cepstrums
+      if (lastCeps != cepstrum) {
+        updateSliderInput(session, 'xWindow', max = xMaxVal, 
+                          value = c(0, xMaxVal))
+        updateSliderInput(session, 'yWindow', min = yMinVal, 
+                          max = yMaxVal, value = c(yMinVal, yMaxVal))
+      }
+      
+
+    }
+    lastCeps <<- cepstrum
+    x
+  }
+  
   placeholder <- read.csv('blank.csv')
+  diffPlacehodler <- data.frame(file=character(), ceps = character(), avg_diff = character())
+  #dataset <- read.csv('blank.csv')
   dataset <- NULL
   
   output$plot <- renderPlot(placeholder %>%
@@ -72,46 +123,88 @@ server <- function(input, output, session) {
   
   output$datasetTable <- renderDataTable(placeholder)
   
+  output$diffTable <- renderDataTable(diffPlacehodler,
+                                      options = list(lengthChange = FALSE))
+  
   observeEvent(input$newFile, {
     newFileMels <- convert_audio(input$newFile$datapath, input$newFile$name)
     
     print(is.null(dataset))
     
     if (!is.null(dataset)) {
-      print("COMBINED")
       dataset <<- dataset %>%
         bind_rows(newFileMels)
     } 
     
     else {
-      print("NOT COMBINED")
       dataset <<- newFileMels
     }
     
+    # no x min necessary, as it will always be 0
     xMaxVal <- max(dataset$frame, na.rm = TRUE)
-    yMaxVal <- ceiling(max(dataset$coeff, na.rm = TRUE)) + 5
+    yMinVal <- floor(min(dataset$coeff, na.rm = TRUE))
+    yMaxVal <- ceiling(max(dataset$coeff, na.rm = TRUE))
     
-    updateSliderInput(session, 'xmin', max = xMaxVal, value = 0)
-    updateSliderInput(session, 'xmax', max = xMaxVal, value = xMaxVal)
-    updateSliderInput(session, 'ymin', max = yMaxVal, value = 0)
-    updateSliderInput(session, 'ymax', max = yMaxVal, value = yMaxVal)
-    updateSelectInput(session, 'ceps', choices = c("Any", unique(dataset$ceps)))
+    updateSliderInput(session, 'xWindow', max = xMaxVal, value = c(0, xMaxVal))
+    updateSliderInput(session, 'yWindow', min = yMinVal, max = yMaxVal, value = c(yMinVal, yMaxVal))
+    updateSelectInput(session, 'ceps', choices = c("All", unique(dataset$ceps)))
     
     print(max(dataset$coeff, na.rm = TRUE))
     
-    output$plot <- renderPlot(dataset %>% 
-                                #filter(ceps == input$ceps) %>%
-                                ggplot(mapping = aes(x = frame, y = coeff, color = file)) +
-                                geom_point(alpha = 0.6) +
-                                xlim(input$xmin, input$xmax) +
-                                ylim(input$ymin, input$ymax) +
+    output$plot <- renderPlot({
+                              mfccPlot <- dataset %>%
+                                filter_data(input$ceps, update = TRUE) %>%
+                                ggplot(mapping = aes(x = frame, y = coeff, color = file))
+                              
+                              if (input$cepstrumShapes) {
+                                mfccPlot <- mfccPlot +
+                                  geom_point(aes(shape = ceps), alpha = input$alpha/100) +
+                                  scale_shape_manual(values = 1:n_distinct(dataset$ceps))
+                                print("LEVELS:")
+                                print(n_distinct(dataset$ceps))
+                                print(length(unique(dataset$ceps)))
+                                print(nlevels(dataset$ceps))
+                              }
+                              
+                              else {
+                                mfccPlot <- mfccPlot + geom_point(alpha = input$alpha/100)
+                              }
+                              
+                              if (input$smoothingLine) {
+                                mfccPlot <- mfccPlot + geom_smooth()
+                              }
+                              
+                              mfccPlot +
+                                xlim(input$xWindow[1], input$xWindow[2]) +
+                                ylim(input$yWindow[1], input$yWindow[2]) +
                                 labs(
                                   x = "Frame (time)",
-                                  y = "Mel Coefficient"
-                                ))
-    output$datasetTable <- renderDataTable(dataset)
-  
+                                  y = "Mel Coefficient",
+                                  color = "File",
+                                  shape = "Cepstrum"
+                                )
+                              })
+    
+    # build/update differences table
+    
+    
+    # update featurs table
+    output$datasetTable <- renderDataTable(dataset %>%
+                                             drop_na() %>%
+                                             mutate(
+                                               coeff = round(coeff, digits = 6)
+                                             )
+                                          )  
+    output$diffTable <- renderDataTable(
+      dataset %>%
+        group_by(file, ceps) %>%
+        summarise(avg_diff = round(abs(mean(coeff, na.rm = TRUE)), digits = 2)),
+      options = list(lengthChange = FALSE)
+      )
+    
   })
+  
+
   
   
 }
